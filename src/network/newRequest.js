e62495ee58e5687fc84007f3426b15d420ad8bd5
snailcsm <908800386@qq.com>
2021-09-06 10:54:51 +0800

    Update newRequest.js

--- src/network/newRequest.js
+++ src/network/newRequest.js
@@ -5,7 +5,7 @@ vue.prototype.$http = axios;
 
 export  function NewRequest(config) {
   const instance=axios.create({
-    baseURL:"http://152.136.185.210:7878/api/m5",
+    baseURL:"后端数据地址",
     timeout:5000
   })
 
